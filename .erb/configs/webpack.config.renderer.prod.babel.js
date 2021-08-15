06acad79c8643c93a0932a86cba371b7c5ffbc76
Julius <julius-makel@online.de>
2021-08-15 16:17:16 +0200

    Fix package by disabling asar in electron build config

--- .erb/configs/webpack.config.renderer.prod.babel.js
+++ .erb/configs/webpack.config.renderer.prod.babel.js
@@ -146,7 +146,7 @@ export default merge(baseConfig, {
      */
     new webpack.EnvironmentPlugin({
       NODE_ENV: 'production',
-      DEBUG_PROD: false,
+      DEBUG_PROD: true,
     }),
 
     new MiniCssExtractPlugin({
